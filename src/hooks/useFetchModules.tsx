205d8cdaf06d37f67d96f0564d36c244e05a1f3c
William ALdair Castro Hilario <wcastroh07@gmail.com>
2024-10-18 18:49:05 -0500

    useFetchModules

--- src/hooks/useFetchModules.tsx
+++ src/hooks/useFetchModules.tsx
@@ -1,75 +1,50 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { Module, useModuleStore } from '../stores/modules/useModuleStore'
 import { ApiFetch } from '../plugins/http/api-fetch'
 import { ModuleModel } from '../models/ModuleModel'
-import { useEffect, useState } from 'react'
 
 const apiFetch = new ApiFetch()
 const moduleModel = new ModuleModel(apiFetch)
 
 export const useFetchModules = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const {
-    modules,
-    loadModules,
-    addModule,
-    updateModule,
-    removeModule,
-    cacheModules,
-    setCacheModules,
-  } = useModuleStore((state) => state)
+  const queryClient = useQueryClient()
 
-  const { dtFin, isFetch } = cacheModules
+  const { loadModules, modules } = useModuleStore((state) => state)
+  // React Query: Fetching modules with caching
+  const { isLoading } = useQuery({
+    queryKey: ['modules'],
+    queryFn: async () => {
+      const listModules = await moduleModel.loadModules()
+      loadModules(listModules)
+      return listModules
+    },
+    staleTime: 1000 * 60 * 60, // 1 hour
+  })
 
-  const fetchModules = async () => {
-    setIsLoading(true)
+  const createModuleMutation = useMutation({
+    mutationFn: (newModule: Module) => moduleModel.saveModule(newModule),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['modules'] })
+    },
+  })
 
-    if (isFetch === false && dtFin === null) {
-      const data = await moduleModel.loadModules()
-      loadModules(data)
-      setCacheModules({ isFetch: true, dtFin: new Date() })
-      setIsLoading(false)
-      return
-    }
+  const modifyModuleMutation = useMutation({
+    mutationFn: (module: Module) => moduleModel.updateModule(module),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['modules'] })
+    },
+  })
 
-    if (isFetch && dtFin) {
-      const diff = new Date().getTime() - dtFin.getTime()
-      const diffInMinutes = diff / 60000
+  const deleteModuleMutation = useMutation({
+    mutationFn: (module: Module) => moduleModel.deleteModule(module.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['modules'] })
+    },
+  })
 
-      if (diffInMinutes < 5) {
-        setIsLoading(false)
-        return
-      }
-    }
-
-    const data = await moduleModel.loadModules()
-    loadModules(data)
-    setCacheModules({ isFetch: true, dtFin: new Date() })
-    setIsLoading(false)
-  }
-
-  useEffect(() => {
-    fetchModules().then()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  const createModule = async (module: Module) => {
-    const newModule = await moduleModel.saveModule(module)
-    addModule(newModule)
-  }
-  const modifyModule = async (module: Module) => {
-    const updatedModule = await moduleModel.updateModule(module)
-    updateModule(updatedModule)
-  }
-
-  const deleteModule = async (module: Module) => {
-    const deletedModule = await moduleModel.deleteModule(module.id)
-    removeModule(deletedModule)
-  }
-
-  const reloadModules = async () => {
-    const data = await moduleModel.loadModules()
-    loadModules(data)
-  }
+  const createModule = (module: Module) => createModuleMutation.mutate(module)
+  const modifyModule = (module: Module) => modifyModuleMutation.mutate(module)
+  const deleteModule = (module: Module) => deleteModuleMutation.mutate(module)
 
   return {
     modules,
@@ -77,6 +52,5 @@ export const useFetchModules = () => {
     createModule,
     modifyModule,
     deleteModule,
-    reloadModules,
   }
 }
